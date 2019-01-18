3a96adc045ee9d845dcd15ffc0b080f53f923ba4
Dmitrii Galantsev <dgalants@iu.edu>
2019-01-18 16:06:15 -0500

    finish basic reader

--- main.cpp
+++ main.cpp
@@ -1,23 +1,46 @@
 #include "mbed.h"
 
-#define RFID_ADDR 0x7D // 0x7D is the default address, 0x7C if you close the address jumper.
-#define I2C_PORTS (0x30) // sda(11) + scl(12)
+#define RFID_ADDR 0xFB // 0x7D is the default address, 0x7C if you close the address jumper.
 
+/* assign pin names */
+PinName sda = port_pin(PortA, I2C_SDA);
+PinName scl = port_pin(PortA, I2C_SCL);
+
+/* attach serial and led */
 Serial pc(SERIAL_TX, SERIAL_RX);
+DigitalOut myled(LED1);
 
-//PortOut scl (PortA, P12);
-//PortOut sda (PortB, P11);
-PinName p2 = port_pin(PortA, 2);
-PinName p1 = port_pin(PortA, 1);
-
-I2C i2c(p1, p2);
+/* init i2c */
+I2C i2c(sda, scl);
 
 int main() {
+
+	pc.printf("\n\nstart\n");
+
+	set_time(2147483638);
+
+	/* 5 byte array */
+	char * data = (char *) calloc(5, 1);
+	char status = 0;
+
     while(1) {
-		//pc.printf("%d\n", i2c.read(0));
-		//pc.printf("HELLO! %d\n", my_io.read());
-		pc.printf("memes\n");
-        wait(1.0); // 1 sec
+		time_t seconds = time(NULL);
+
+		/* read rfid tag */
+		if(i2c.read(RFID_ADDR, data, 5))
+			pc.printf("[WARN] i2c read returned non-zero!\n");
+
+		/* set status (0 if no rfid found) */
+		status = data[0] | data[1] | data[2] | data[3] | data[4];
+		myled = status;
+		//pc.printf("s: %d    ", status);
+
+		/* only print rfid data in hex if any data read */
+		if (status)
+			pc.printf("d : \"%02x%02x%02x%02x%02x\"\n", data[0], data[1], data[2], data[3], data[4]);
+
+		/* sleep for 0.2 sec */
+        wait(0.2);
     }
 
 	return 0;
